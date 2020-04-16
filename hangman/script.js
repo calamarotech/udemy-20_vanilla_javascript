889679091c5053535b904154713468edbcd617c0
shellbacklab <47547537+shellbacklab@users.noreply.github.com>
2020-04-15 21:29:48 -0500

    hangman complete

--- hangman/script.js
+++ hangman/script.js
@@ -31,11 +31,86 @@ function displayWord() {
 
   const innerWord = wordEl.innerText.replace(/\n/g, '');
 
+  // check win condition
   if (innerWord === selectedWord) {
     finalMessage.innerText = 'Congratulations! You Won! 😀';
     popup.style.display = 'flex';
   }
 }
 
+// update the wrong letters
+function updateWrongLettersEl() {
+  // display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+
+  // display figure parts
+  figureParts.forEach((part, index) => {
+    const error = wrongLetters.length;
+
+    if (index < error) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost. 😥';
+    popup.style.display = 'flex';
+  }
+}
+
+// show notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+// keydown letter press
+window.addEventListener('keydown', (e) => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
+// play again event listener
+playAgainBtn.addEventListener('click', () => {
+  // empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
+});
+
 displayWord();
-console.log(selectedWord);
